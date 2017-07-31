02925ab379561ad8005f919ef224416a75a2e983
pawelkupka <pawel.kupka@gmail.com>
2017-07-31 17:50:14 +0200

    Comment

--- Creational/Prototype/Program.cs
+++ Creational/Prototype/Program.cs
@@ -7,7 +7,7 @@
             var prototype1 = new ConcretePrototype1();
             var prototype2 = new ConcretePrototype2();
 
-            var prototype = prototype1.Clone();
+            var prototype = prototype1.Clone(); // Copy an existing prototype instead of creating a new one
             prototype.DisplayState();
 
             prototype = prototype2.Clone();
