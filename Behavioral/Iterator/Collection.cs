c75526c8c5bb4a67761251cd93276c74a727538e
pawelkupka <pawel.kupka@gmail.com>
2017-08-17 20:39:20 +0200

    iterator pattern added

---
+++ Behavioral/Iterator/Collection.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+
+namespace DesignPatterns.Behavioral.Iterator
+{
+    public class Collection : IEnumerable
+    {
+        private readonly ArrayList _elements;
+
+        public Collection()
+        {
+            _elements = new ArrayList();
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            foreach (var element in _elements)
+                yield return element;
+        }
+
+        public void Add(string element)
+        {
+            _elements.Add(element);
+        }
+    }
+}
--- Behavioral/Iterator/Program.cs
+++ Behavioral/Iterator/Program.cs
@@ -1,15 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Iterator
+namespace DesignPatterns.Behavioral.Iterator
 {
     class Program
     {
         static void Main(string[] args)
         {
+            // Iterator pattern is used to traverse a container and access the container's elements
+            var collection = new Collection() { "element1", "element2", "element3" };
+            foreach (var element in collection)
+                Console.WriteLine(element);
         }
     }
 }
